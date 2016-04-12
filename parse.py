4b7e40369d61a2121b6cb41974ef4c0a680f587e
Guido De Rosa <guidoderosa@gmail.com>
2016-04-12 20:35:55 +0100

    print texts

--- parse.py
+++ parse.py
@@ -5,4 +5,4 @@ parser = lxml.html.parse('input.html')
 all_links = parser.findall('//a')
 
 for idx, val in enumerate(all_links):
-    print(idx, val)
+    print(idx, val.__class__.__name__, val.text)
