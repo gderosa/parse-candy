ef15ab2f7ae67132dbe1d349263bc1fc586e04ad
Guido De Rosa <guidoderosa@gmail.com>
2016-04-12 20:58:06 +0100

    re

--- parse.py
+++ parse.py
@@ -1,8 +1,13 @@
+import re
 import lxml.html
+
+IS_EPISODE = re.compile('Episodio (\d{1,3})')
 
 parser = lxml.html.parse('input.html')
 
 all_links = parser.findall('//a')  # list of lxml.html.HtmlElement objects
 
 for idx, a in enumerate(all_links):
-    print(idx, a.text, a.get('href'))
+    matches = IS_EPISODE.findall(a.text)
+    if matches:
+        print(idx, matches, a.text, a.get('href'))
